86b8fb2bf63fc5306b305bc2cc375c29b0fc61c8
Yordi Lorenzo de Kleijn <yordilorenzo@gmail.com>
2015-12-04 15:51:26 +0100

    Improved method prototype and made cb to callback to be more descriptive

--- framework.js
+++ framework.js
@@ -51,13 +51,14 @@ var cache = {
 	templates : {}
 }
 
-Number.prototype.isSuccessful = function() {
+Object.prototype.isSuccessful = function() {
 	if (this.status == undefined) return false;
 	return this.status >= 200 && this.status < 400;
 }
 
+
 var get = {
-	template : function(settings, cb){
+	template : function(settings, callback){
 		settings['name'] = settings['name'] || false;
 		settings['data'] = settings['data'] || false;
 
@@ -79,20 +80,20 @@ var get = {
 
 		if(!settings['name']){
 			console.error('No template specified');
-			cb();
+			callback();
 			return false;
 		}
 		else if(settings['name'] in cache.templates){
-			cb(fillTemplate(cache.templates[settings['name']]));
+			callback(fillTemplate(cache.templates[settings['name']]));
 		}
 		else{
 			var request = new XMLHttpRequest();
 			request.open('GET', '/templates/'+settings['name']+'.html', true);
 			request.onload = function() {
-				if (request.status.isSuccessful()) {
+				if (request.isSuccessful()) {
 					var template = request.responseText;
 					cache.templates[settings['name']] = template;
-					cb(fillTemplate(template));
+					callback(fillTemplate(template));
 				}
 				else console.error('Error loading data');
 			};
@@ -108,7 +109,7 @@ var get = {
 		var request = new XMLHttpRequest();
 		request.open('GET', url, true);
 		request.onload = function() {
-			if (request.status.isSuccessful()) {
+			if (request.isSuccessful()) {
 				var res = false;
 				try{
 			        res = JSON.parse(request.responseText);
