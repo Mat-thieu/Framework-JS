ac284a3425510596151753750c10170b8dea9d0a
unknown <matthieu@webvalue.nl>
2015-12-09 14:11:05 +0100

    DEV

--- api.php
+++ api.php
@@ -1,6 +1,21 @@
 <?php
 	header('Content-type: application/json');
 
-	echo json_encode(array("stuff" => "Herro"));
+	echo json_encode(
+		array("items" =>
+			array(
+				'one',
+				'two',
+				'three'
+			),
+		"other" =>
+			array(
+				"herp",
+				"derp",
+				"ferp"
+			),
+		"test" => "Hello World"
+		)
+	);
 
 ?>
